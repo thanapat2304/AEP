8d121be29e1cb20e3066311288f72af99f20a025
thanapat2304 <thanapat.sop@gmail.com>
2024-08-28 12:34:56 +0700

    TEST APP

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for, send_file,
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():    
     return render_template("index.html")
